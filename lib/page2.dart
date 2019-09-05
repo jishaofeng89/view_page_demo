0247e2a76bbd09be839f34d841d9f885d708f551
jishaofeng89 <1195957057@qq.com>
2019-09-05 12:07:53 +0800

    jump

--- lib/page2.dart
+++ lib/page2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:view_page_demo/page3.dart';
 
 class Page2 extends StatefulWidget {
   Page2({Key key}) : super(key: key);
@@ -8,11 +9,20 @@ class Page2 extends StatefulWidget {
 
 class _Page2State extends State<Page2> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      return Page3();
+    }));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-       child: Center(
-         child: Text('第二页'),
-       ),
+      child: Center(
+        child: Text('第二页'),
+      ),
     );
   }
 }
---
+++ lib/page3.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Page3 extends StatefulWidget {
+  Page3({Key key}) : super(key: key);
+
+  _Page3State createState() => _Page3State();
+}
+
+class _Page3State extends State<Page3> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Center(
+         child: Text('第三页'),
+       ),
+    );
+  }
+}
